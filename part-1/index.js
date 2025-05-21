a790fa5ade34e4b7ab623ef42d7e5530cde1a39c
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-05-22 02:28:38 +0600

    second

---
+++ part-1/index.js
@@ -0,0 +1,35 @@
+const applePrice = "20.300";
+const nokia = " ";
+const applePriceNum = parseFloat(applePrice);
+const number = 10 / " hr";
+console.log(applePriceNum + nokia);
+console.log(nokia);
+
+console.log(Number.isNaN(NaN));
+
+console.log(number);
+
+const mony = 1000;
+const apple = 400;
+const orange = 300;
+const total = apple + orange;
+const totalExpanse = mony - total;
+console.log(totalExpanse);
+
+const a = isNaN("11");
+const b = isNaN(2 - 10);
+console.log(a);
+console.log(b);
+
+const num = 111;
+const modules = num % 2;
+console.log(modules);
+
+const marks = [75.25, 65, 80, 35.45, 99.5];
+
+let sum = 0;
+
+for (let i = 0; i < marks.length; i++) {
+  sum = sum + marks[i];
+}
+console.log(sum);
