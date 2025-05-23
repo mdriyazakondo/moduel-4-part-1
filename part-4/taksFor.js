13079b70529340b699d8c26fa7200f7ff0967df1
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-05-23 11:42:00 +0600

    part-4

---
+++ part-4/taksFor.js
@@ -0,0 +1,39 @@
+let num = 0;
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+// for (let i = 78; i < 98; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+let sum = 0;
+// for (let num = 91; num < 129; num++) {
+//   if (num % 2 == 1) {
+//     sum = sum + num;
+//     console.log(num);
+//     console.log(sum);
+//   }
+// }
+
+// for (let num = 51; num < 85; num++) {
+//   if (num % 2 == 0) {
+//     sum = sum + num;
+//     console.log(num);
+//     console.log(sum);
+//   }
+// }
+
+// for (let num = 0; num < 9; num++) {
+//   sum = sum + num;
+//   console.log(sum);
+// }
+
+// console.log(sum);
+
+for (let num = 81; num >= 65; num--) {
+  console.log(num);
+}
