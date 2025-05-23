64e942580464b982c06ed83a7b372a6e6df0e73d
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-05-23 13:47:43 +0600

    loop

---
+++ part-4/brack.js
@@ -0,0 +1,40 @@
+let num = 100;
+
+// while (num > 50) {
+//   console.log(num);
+//   if (num < 75) {
+//     break;
+//   }
+//   num--;
+// }
+// let sum = 0;
+// for (let i = 0; i < 100; i++) {
+//   sum = sum + i;
+//   if (sum >= 100) {
+//     break;
+//   }
+//   // console.log(sum);
+// }
+
+// console.log(sum);
+
+// for (let i = 1; i <= 100; i++) {
+//   if (Math.sqrt(i) % 2 === 0) {
+//     console.log(i);
+//     break;
+//   }
+// }
+
+// for (let i = 1; i < 40; i++) {
+//   if (i % 2 == 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+for (let i = 1; i < 90; i++) {
+  if (i % 5 == 0) {
+    continue;
+  }
+  console.log(i);
+}
