f7869ec40b1e78ae3eb6f83f973f05fd6a865dbd
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-05-23 10:55:43 +0600

    part-4

---
+++ part-4/for-of.js
@@ -0,0 +1,10 @@
+const numbers = [12, 34, 55, 77, 99, 45, 75];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+console.log("hello");
+
+for (const num of numbers) {
+  console.log(num);
+}
---
+++ part-4/while.js
@@ -0,0 +1,14 @@
+const number = 7;
+
+if (number < 1) {
+  console.log("soto akta number paisi");
+}
+
+console.log("something is rang");
+
+let num = 0;
+
+while (num < 5) {
+  console.log("looping is running");
+  num++;
+}
